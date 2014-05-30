39c4aa4e869619c0a066f2fa998d3185f390faab
Usuario <portatil@portatil.(none)>
2014-05-30 13:18:26 +0200

    adios mundo añadido

--- hola_mundo.py
+++ hola_mundo.py
@@ -3,4 +3,9 @@ def hola_mundo(i):
    for e in range(0,i):
       print "hola mundo"
 
-hola_mundo(6)
+def adios_mundo(i):
+   for e in range(0,i):
+      print "adios mundo"
+
+hola_mundo(1)
+adios_mundo(2)
